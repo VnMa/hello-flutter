b6e06f147f8071634a019e7a7e46e470b9d3e75f
An Vo <akira.an111@gmail.com>
2019-04-22 17:02:00 +0700

    building initial layouts

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,116 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()) ;
 
 class MyApp extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+        child: Row (
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildButtonColumn(color, Icons.call, 'Call'),
+              _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+              _buildButtonColumn(color, Icons.share, 'SHARE'),
+            ]
+        )
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      )
+    );
+
     return MaterialApp(
-      title: 'Startup Name Generator',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: RandomWords(),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => new RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved)  {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          final Iterable<ListTile> tiles = _saved.map(
-                (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final List<Widget> divided = ListTile
-              .divideTiles(
-            context: context,
-            tiles: tiles,
-          )
-              .toList();
-
-          return Scaffold(         // Add 6 lines from here...
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter demo'),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Image.asset('images/lake.jpg',
+            width: 600,
+            height: 240,
+                fit: BoxFit.cover
             ),
-            body: ListView(children: divided),
-          );                       // ... to here.
-        },
-      ),
+            titleSection,
+            buttonSection,
+            textSection
+          ],
+        )
+      )
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-    appBar: AppBar(
-      title: Text('Startup Name Generator'),
-      actions: <Widget>[
-        IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color
+            )
+          )
+        )
       ]
-    ),
-    body: _buildSuggestions(),
-  );
+    );
   }
+
+
+  Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        'Campground',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold
+                        ),
+                      )
+                  ),
+                  Text(
+                      'Kandersteg, Switzerland',
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      )
+                  )
+                ],
+              )
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('411')
+        ],
+      )
+  );
 }
